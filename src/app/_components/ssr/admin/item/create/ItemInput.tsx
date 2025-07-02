d1f2da06d7c2c230902735dae0e1e86830e60f22
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-02 14:20:05 +0100

    Created a component to input a value

--- src/app/_components/ssr/admin/item/create/ItemInput.tsx
+++ src/app/_components/ssr/admin/item/create/ItemInput.tsx
@@ -1,8 +1,22 @@
+import { Input } from '@/app/_components/ui/input';
+import { capitaliseFirstLetter } from '@/app/_lib/utils/usefulFunctions';
+import { Label } from '@radix-ui/react-label';
+
 interface ItemInputProps {
 	type: 'title' | 'description' | 'price' | 'link';
 	setValue: (value: any) => void;
 }
 
 export default function ItemInput({ type, setValue }: ItemInputProps) {
-	return <></>
+	return (
+		<div className="grid w-full max-w-sm items-center gap-1">
+			<Label>{capitaliseFirstLetter(type)}</Label>
+			<Input
+				type={type === 'price' ? 'number' : 'text'}
+				id={type}
+				placeholder={capitaliseFirstLetter(type)}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</div>
+	);
 }
