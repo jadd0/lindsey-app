ba5ca67e064343cc5da6413a1bb624980b444bc3
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-04 14:47:01 +0100

    New function for backend auth when not used as middleware

--- src/app/_lib/auth/backendAuth.ts
+++ src/app/_lib/auth/backendAuth.ts
@@ -1,6 +1,6 @@
 import admin from "../firebase/admin";
 
-export default async function handler(req: any, res: any) {
+export async function authHandlerReq(req: any, res: any) {
 	const idToken = req.headers.authorization?.split('Bearer ')[1];
 	if (!idToken) {
 		return res.status(401).json({ error: 'No token provided' });
@@ -12,5 +12,19 @@ export default async function handler(req: any, res: any) {
 		return res.status(200).json({ uid: decodedToken.uid });
 	} catch (error) {
 		return res.status(401).json({ error: 'Invalid token' });
+	}
+}
+
+export async function authHandlerFn(req: any) {
+	const idToken = req.headers.authorization?.split('Bearer ')[1];
+	if (!idToken) {
+		return false;
+	}
+
+	try {
+		const decodedToken = await admin.auth().verifyIdToken(idToken);
+		return decodedToken.uid
+	} catch (error) {
+		return false;
 	}
 }
