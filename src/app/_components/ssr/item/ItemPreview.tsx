788dc6a02b061eb52878a6c3cb337e4654733cec
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-07 14:40:51 +0100

    Created files for item previews

---
+++ src/app/_components/ssr/item/ItemPreview.tsx
@@ -0,0 +1,9 @@
+import { Item } from '@/app/_shared/types';
+
+export default function ItemPreview(Item: Item) {
+	return (
+		<div className="flex flex-col items-center border border-white-800">
+      
+    </div>
+	);
+}
