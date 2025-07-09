7d3d0061818c404998469b08bf53e920ba499067
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-09 16:45:53 +0100

    Redesigned navbar

--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -1,22 +1,21 @@
+import Link from 'next/link';
+
 export default function Navbar() {
 	return (
-		<nav className="w-screen h-10">
-			<div className="container mx-5 p-5 flex justify-between items-center absolute top-0">
-				<a href="/" className="text-black text-lg font-bold">
+		<div className="w-full flex items-center h-10 p-5 pt-10">
+			<Link href="/">
+				<h1 className="text-2xl font-bold ml-10 hover:text-gray-400 duration-150 ease-in">
 					Home
-				</a>
-				<div>
-					<a href="/about" className="text-gray-300 hover:text-black px-3 py-2">
-						Shop
-					</a>
-					<a
-						href="/contact"
-						className="text-gray-300 hover:text-black px-3 py-2"
-					>
-						Contact
-					</a>
-				</div>
+				</h1>
+			</Link>
+			<div className="ml-auto flex space-x-8 mr-5">
+				<Link href="/items">
+					<h1 className="text-2xl font-bold hover:text-gray-400 duration-150 ease-in">Items</h1>
+				</Link>
+				<Link href="/contact">
+					<h1 className="text-2xl font-bold hover:text-gray-400 duration-150 ease-in">Contact</h1>
+				</Link>
 			</div>
-		</nav>
+		</div>
 	);
 }
