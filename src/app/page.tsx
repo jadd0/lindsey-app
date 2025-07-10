8699227b397f7649c62d39c5fb674c32bdb51617
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-10 12:00:48 +0100

    Incorporated artwork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
+
 export default function HomePage() {
 	return (
 		<div className="flex h-screen w-full items-center justify-center">
+			<div className="relative w-220 h-auto aspect-[16/14]">
+				<Image
+					src="/LindseyShopHangingSign.png"
+					alt="Lindsey Shop Logo"
+					fill
+					className="object-cover rounded"
+				/>
+			</div>
 		</div>
 	);
 }
