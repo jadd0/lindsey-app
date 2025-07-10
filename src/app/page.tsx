51bc3d66fd03582bf46148d4875ed39fd3481ac0
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-10 14:56:22 +0100

    Added a new section for "the story"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,32 @@ export default function HomePage() {
 					/>
 				</div>
 			</div>
+			{/* Our story */}
+			<div className="grid grid-cols-2 w-full space-between mt-30">
+				{/* Image */}
+				<div className="flex items-center justify-center">
+					<Image
+						src="/hangingTree.png"
+						alt="Bouganvillea Tree with Hanging Decoration"
+						width={500}
+						height={500}
+						className="rounded-full"
+					/>
+				</div>
+				{/* Text */}
+				<div className="flex flex-col justify-center pt-15">
+					<h2 className="text-4xl font-bold">All About The Story:</h2>
+					<p className="mt-4 max-w-200">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, officia
+						laborum natus fugiat vitae praesentium quos, possimus molestiae
+						autem quisquam necessitatibus dolorum dicta quis neque cumque at
+						soluta. A, saepe? Lorem ipsum dolor, sit amet consectetur
+						adipisicing elit. Tempore, laudantium totam. A adipisci aut rerum
+						est. Adipisci aliquid voluptas sequi eum, rerum accusantium nihil
+						veniam, commodi maxime inventore quis molestiae?
+					</p>
+				</div>
+			</div>
 			{/* Featured content */}
 		</div>
 	);
